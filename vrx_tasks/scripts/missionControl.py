cf3471a1168286fedda6098cf121a0cc56ae6d78
Benjamiiiin <bensteer0@gmail.com>
2019-09-25 21:25:38 +1000

    Merged stable_steven's vrx_tasks into control_ben. Made a few modifications, including setting the waypoints for the wayfinding task to be NAV_STATIONs instead of NAV_WAYPOINTS, and giving them short durations.

---
+++ vrx_tasks/scripts/missionControl.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Checks info messages to determine what needs to be done.
+
+import rospy
+from vrx_gazebo.msg import Task
+params = {
+    "taskTopic": "/waypoints",
+}
+
+rospy.init_node("missionControl.py",anonymous=True)
+
+for i in params:
+    params[i] = rospy.get_param('~'+i, params[i])
+
+def cb(data):
+    if data.name=="something":
+        # do something
+
+pub = rospy.Subscriber(params['taskTopic'], Task,cb);
+
+def cb(data):
+    print("RECEIVED WAYPOINT MESSAGE")
+    global pub
+    global listener
+    route = WaypointRoute()
+    waypoints = []
+    for dps in data.poses:
+        wp=Waypoint()
+        wp.pose=dps.pose # it starts as a posestamped so extract the pose
+        wp.nav_type=wp.NAV_STATION
+        waypoints.append(wp)
+    route.waypoints=waypoints
+    route.speed=params["speed"]
+    pub.publish(route)
+
+sub = rospy.Subscriber(params['inTopic'], Path, cb)
+rospy.spin()
